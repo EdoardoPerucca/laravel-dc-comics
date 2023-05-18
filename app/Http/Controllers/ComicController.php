72dbb74a5f7a94377d46b6483400ebf65f78d22f
EdoardoPerucca <edoardo.perucca@hotmail.com>
2023-05-18 17:12:05 +0200

    validator created with traslate

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Comic;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class ComicController extends Controller
 {
@@ -37,6 +38,8 @@ class ComicController extends Controller
      */
     public function store(Request $request)
     {
+        $this->validation($request);
+
         $formComic = $request->all();
 
         $newComic = new Comic();
@@ -81,6 +84,8 @@ class ComicController extends Controller
      */
     public function update(Request $request, Comic $comic)
     {
+        $this->validation($request);
+
         $formComic = $request->all();
 
         $comic->update($formComic);
@@ -106,5 +111,37 @@ class ComicController extends Controller
     public function delete(Comic $comic)
     {
         return view('comics.delete', compact($comic));
+    }
+
+    private function validation($request)
+    {
+        $formData = $request->all();
+
+        $validator = Validator::make($formData, [
+            'title' => 'required|max:50|min:5',
+            'description' => 'required',
+            'thumb' => 'required',
+            'price' => 'required|max:6',
+            'series' => 'required|max:50',
+            'sale_date' => 'required|max:10',
+            'type' => 'required|max:50',
+        ], [
+            'title.required' => 'Il titolo è obligatorio',
+            'title.max' => 'Il titolo non può essere più corto di 50 caratteri',
+            'title.min' => 'Il titolo non può essere più corto di 5 caratteri',
+            'description.required' => 'La descrizione è obligatoria',
+            'thumb.required' => 'L immagine è obligatoria',
+            'price.required' => 'Il prezzo è obligatorio',
+            'price.max' => 'Il prezzo non deve essere più lungo di 6 caratteri',
+            'series.required' => 'La serie è obligatoria',
+            'series.max' => 'La serie non può essere più corta di 50 caratteri',
+            'sale_date.required' => 'La data è obligatoria',
+            'sale_date.max' => 'La data non può essere più corta di 10 caratteri',
+            'type.required' => 'Il tipo è obligatorio',
+            'type' => 'Il tipo non può essere più lungo di 50 caratteri',
+
+        ])->validate();
+
+        return $validator;
     }
 }
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,18 +1,70 @@
 <footer>
     <div class="container-1000">
 
-        <div class="footer-left">
-
-            <div class="row">
-
-               
+        <div id="footer-top">
+            <div class="footer-top-inner" class="container">
+      
+                <ul class="footer-links">
+                    <li>
+                        <strong>DC Comics</strong>
+                        <ul>
+                            <li>Characters</li>
+                            <li>Comics</li>
+                            <li>Movies</li>
+                            <li>TV</li>
+                            <li>Games</li>
+                            <li>Videos</li>
+                            <li>News</li>
+                        </ul>
+                    </li>
+      
+                    <li>
+                        <strong>Shop</strong>
+                        <ul>
+                            <li>Shop DC</li>
+                            <li>Shop DC Collectibles</li>
+                        </ul>
+                    </li>
+      
+                    <li>
+                        <strong>DC</strong>
+                        <ul>
+                            <li>Terms Of Use</li>
+                            <li>Privacy Policy</li>
+                            <li>Terms Of Use</li>
+                            <li>Advertising</li>
+                            <li>Jobs</li>
+                            <li>Subscriptions</li>
+                            <li>Talent Workshops</li>
+                            <li>CPSC Certificates</li>
+                            <li>Ratings</li>
+                            <li>Shop Help</li>
+                            <li>Contact Us</li>
+                        </ul>
+                    </li>
+                    <li>
+                        <strong>Sites</strong>
+                        <ul>
+                            <li>DC</li>
+                            <li>MAD Magazine</li>
+                            <li>DC Kids</li>
+                            <li>DC Universe</li>
+                            <li>DC Power Visa</li>
+                        </ul>
+                    </li>
+                </ul>
+      
+                
+                <div class="footer-right col-6">
+                    <img src="{{ Vite::asset('resources/img/dc-logo-bg.png') }}" alt="Logo">
+                </div>
             </div>
         </div>
+    
+    
 
+       
 
-        <div class="footer-right">
-            <img src="{{ Vite::asset('resources/img/dc-logo-bg.png') }}" alt="Logo">
-        </div>
 
         
     </div>
